badd5983617ace9e32b5a580286c5db376f92277
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-12-08 12:33:42 -0500

    Add bootstrap

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -2,9 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-persons',
-    template: `<h1>Persons list</h1>
-    <app-person></app-person>
-    <app-person></app-person>`
+    templateUrl: './persons.component.html',
+    styleUrls: ['./persons.component.css']
 })
 export class PersonsComponent{
 
