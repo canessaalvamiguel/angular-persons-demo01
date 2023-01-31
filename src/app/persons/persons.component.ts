557d6ad8289431fb901fd5276083827432d76d9b
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-01-31 13:21:51 -0500

    Format files

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -7,16 +7,14 @@ import { Person } from "../Person.model";
     styleUrls: ['./persons.component.css']
 })
 export class PersonsComponent{
-    message = "Persons added";
-    show = false;
+    message = "There is data";
     title = "Persons list"
 
-    persons: Person[] = [];
+    persons: Person[] = [new Person("Jose", "Perez"), new Person("Miguel", "Canessa")];
     nameInput: string = "";
     lastNameInput: string = "";
 
     addPerson(){
-        this.show = true;
         this.message = "Person was added";
         let person1 = new Person(this.nameInput, this.lastNameInput);
         this.persons.push(person1);
