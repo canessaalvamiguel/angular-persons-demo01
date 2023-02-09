e554b2c95f2d5b59b98ff0bacaad281fd1949fd7
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-02-09 15:49:29 -0500

    Revert "Implement Child Routes"
    This reverts commit 6dc2c8137f5604ba2e20fd933f4ff419bdc829d4.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './error/error.component';
 import { FormPersonComponent } from './persons/form-person/form-person.component';
 import { PersonsComponent } from './persons/persons.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   { path: '', component: PersonsComponent },
-  {
-    path: 'persons',
-    component: PersonsComponent,
-    children: [
-      { path: 'add', component: FormPersonComponent },
-      { path: ':id', component: FormPersonComponent },
-    ],
-  },
-  {path: '**', component: ErrorComponent}
+  { path: 'persons', component: PersonsComponent },
+  { path: 'persons/add', component: FormPersonComponent },
+  { path: 'persons/:id', component: FormPersonComponent },
+  { path: '**', component: ErrorComponent },
 ];
 
 @NgModule({
