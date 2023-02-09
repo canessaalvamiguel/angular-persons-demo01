6dc2c8137f5604ba2e20fd933f4ff419bdc829d4
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-02-09 15:44:39 -0500

    Implement Child Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 import { FormPersonComponent } from './persons/form-person/form-person.component';
 import { PersonsComponent } from './persons/persons.component';
 
 const routes: Routes = [
-  { path: '', component: PersonsComponent},
-  { path: 'persons', component: PersonsComponent},
-  { path: 'persons/add', component: FormPersonComponent},
-  { path: 'persons/:id', component: FormPersonComponent}
+  { path: '', component: PersonsComponent },
+  {
+    path: 'persons',
+    component: PersonsComponent,
+    children: [
+      { path: 'add', component: FormPersonComponent },
+      { path: ':id', component: FormPersonComponent },
+    ],
+  }
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forRoot(
-      routes
-    )
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
