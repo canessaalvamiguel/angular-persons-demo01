dca09a1cee1f3a883f8262ef9280f678d8c4758b
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-02-10 17:53:52 -0500

    Implement Guardian for protected Urls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { FormPersonComponent } from './persons/form-person/form-person.component
 import { PersonsComponent } from './persons/persons.component';
 import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
+import { LoginGuardian } from './login/login-guardian.service';
 
 const routes: Routes = [
-  { path: '', component: PersonsComponent },
-  { path: 'persons', component: PersonsComponent },
-  { path: 'persons/add', component: FormPersonComponent },
-  { path: 'persons/:id', component: FormPersonComponent },
+  { path: '', component: PersonsComponent, canActivate:[LoginGuardian] },
+  { path: 'persons', component: PersonsComponent, canActivate:[LoginGuardian] },
+  { path: 'persons/add', component: FormPersonComponent, canActivate:[LoginGuardian] },
+  { path: 'persons/:id', component: FormPersonComponent, canActivate:[LoginGuardian] },
   { path: 'login', component: LoginComponent },
   { path: '**', component: ErrorComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DataServices } from './data.services';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './login/login.service';
+import { LoginGuardian } from './login/login-guardian.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,7 @@ import { LoginService } from './login/login.service';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [LoggingService, PersonsService, DataServices, LoginService],
+  providers: [LoggingService, PersonsService, DataServices, LoginService, LoginGuardian],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
