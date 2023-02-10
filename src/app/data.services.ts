cd51c6474a362912876f84017730aa3e5c6bb81f
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-02-10 17:19:23 -0500

    Add token to http calls

--- src/app/data.services.ts
+++ src/app/data.services.ts
@@ -1,21 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginService } from './login/login.service';
 import { Person } from './Person.model';
 
 @Injectable()
 export class DataServices {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private loginService: LoginService
+  ) {}
 
   loadPersons() {
+    const token = this.loginService.getIdToken();
     return this.httpClient.get<Person[]>(
-      'https://angularapp-persons-list-default-rtdb.firebaseio.com/data.json'
+      'https://angularapp-persons-list-default-rtdb.firebaseio.com/data.json?auth=' +
+        token
     );
   }
 
   savePerson(persons: Person[]) {
+    const token = this.loginService.getIdToken();
     this.httpClient
       .put(
-        'https://angularapp-persons-list-default-rtdb.firebaseio.com/data.json',
+        'https://angularapp-persons-list-default-rtdb.firebaseio.com/data.json?auth=' +
+          token,
         persons
       )
       .subscribe(
@@ -27,10 +35,12 @@ export class DataServices {
   }
 
   updatePerson(index: number, person: Person) {
+    const token = this.loginService.getIdToken();
     let url: string =
       'https://angularapp-persons-list-default-rtdb.firebaseio.com/data/' +
       index +
-      '.json';
+      '.json?auth=' +
+      token;
     this.httpClient.put(url, person).subscribe(
       (response) => console.log('Result of updating Person ' + response),
       (error) => console.log('Error while updating persons: ' + error)
@@ -38,10 +48,12 @@ export class DataServices {
   }
 
   deletePerson(index: number) {
+    const token = this.loginService.getIdToken();
     let url: string =
       'https://angularapp-persons-list-default-rtdb.firebaseio.com/data/' +
       index +
-      '.json';
+      '.json?auth=' +
+      token;
     this.httpClient.delete(url).subscribe(
       (response) => console.log('Result of deleting Person ' + response),
       (error) => console.log('Error while deleting persons: ' + error)
